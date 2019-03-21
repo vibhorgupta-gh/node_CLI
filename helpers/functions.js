c3502e799662956df9aae75ce20af06c8e78f2a3
VibhorCodecianGupta <guptavibhu98@gmail.com>
2019-03-21 15:58:16 +0530

    API working

--- helpers/functions.js
+++ helpers/functions.js
@@ -2,22 +2,28 @@ const Pair = require('../models/pair')
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const get = (key) => {
-  Pair.findOne({key: key}).then(pair => {
-    console.log(pair)
-  })
+const get = async (key) => {
+  try {
+    const pair = await Pair.findOne({key: key})
+    return pair
+  } catch (e) {
+      console.error(e.message)
+  }
 }
 
-const set = (key, value) => {
-  if (get(key)) {
-    Pair.findOneAndUpdate({key: key}, {value: value}).then(pair => {
-      console.log(pair)
-    })
-  }
-  else {
-    Pair.create({key: key, value: value}).then(pair => {
-      console.log(pair)
-    })
+const set = async (key, value) => {
+  try {
+    const exists = await Pair.findOne({key: key})
+    let pair
+    if (exists) {
+      pair = Pair.findOneAndUpdate({key: key})
+    }
+    else {
+      pair = Pair.create({key: key, value: value})
+    }
+    return pair
+  } catch(e) {
+      console.error(e.message)
   }
 }
 
--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
 const {get, set} = require('../helpers/functions')
@@ -6,12 +5,12 @@ const {get, set} = require('../helpers/functions')
 
 router.get('/get/:key', async (req, res, next) => {
   let pair = await get(req.params.key)
-  res.json(pair)
+  res.json({value: pair.value})
 })
 
-router.get('/set/:key_:value', async (req, res, next) => {
+router.get('/set/:key/:value', async (req, res, next) => {
   let pair = await set(req.params.key, req.params.value)
-  res.json(pair)
+  res.json({key: pair.key, value: pair.value})
 })
 
 module.exports = router
