09f752c4d93d0b384e9c8616e750a0df451cfb6e
VibhorCodecianGupta <guptavibhu98@gmail.com>
2019-03-22 12:50:54 +0530

    increases coverage

--- test/test.js
+++ test/test.js
@@ -11,18 +11,26 @@ describe('Function testing', () => {
   let key = 'a'
   let value = '6'
 
-  it('set method test', (done) => {
+  it('set method test: true', (done) => {
     let pair = set(key, value)
-    let new_pair = set('arbitrary_key', 'arbitrary_value')
     pair.should.be.fulfilled
+    done()
+  })
+
+  it('set method test: false', (done) => {
+    let new_pair = set('arbitrary_key', 'arbitrary_value')
     new_pair.should.be.fulfilled
     done()
   })
 
-  it('get method test', (done) => {
+  it('get method test: true', (done) => {
     let pair = get(key)
-    let no_pair = get('key')
     pair.should.be.fulfilled
+    done()
+  })
+
+  it('get method test: false', (done) => {
+    let no_pair = get('key')
     no_pair.should.be.fulfilled
     no_pair.should.eventually.equal(false)
     done()
@@ -32,13 +40,17 @@ describe('Function testing', () => {
 describe('Util testing', () => {
   let key = 'a'
 
-  it('update util test', (done) => {
-    let falsy = isUpdated(key, 'a')
+  it('update util test: true', (done) => {
     let truthy = isUpdated(key, 'b')
-    falsy.should.be.fulfilled
     truthy.should.be.fulfilled
-    falsy.should.eventually.equal(false)
     truthy.should.eventually.equal(true)
+    done()
+  })
+
+  it('update util test: false', (done) => {
+    let falsy = isUpdated(key, 'a')
+    falsy.should.be.fulfilled
+    falsy.should.eventually.equal(false)
     done()
   })
 })
